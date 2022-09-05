4afd4bba068464c1db391e8c104b9aa64438b098
MattWrightIWF <111991781+MattWrightIWF@users.noreply.github.com>
2022-09-05 13:42:56 +0100

    updates

--- setupscripts.bat
+++ setupscripts.bat
@@ -5,23 +5,25 @@ SET ThisScriptsDirectory=%~dp0
 
 ECHO IWF PC Setup Scripts
 ECHO ===================================
-ECHO 1 - Rename, Expand
-ECHO 2 - Join Local Domain
-ECHO 3 - Join Azure Domain
-ECHO 4 - Install Apps
-ECHO 5 - Encrypt Drive
-ECHO 6 - Re-Backup Bitlocker recovery key
+ECHO 1 - Update Scripts
+ECHO 2 - Rename, Expand
+ECHO 3 - Join Local Domain
+ECHO 4 - Join Azure Domain
+ECHO 5 - Install Apps
+ECHO 6 - Encrypt Drive
+ECHO 7 - Re-Backup Bitlocker recovery key
 
 ECHO ===================================
 
-CHOICE /C 123456 /M "Press corresponding number to start the script"
+CHOICE /C 1234567 /M "Press corresponding number to start the script"
 
-IF ERRORLEVEL 6 GOTO backup-key
-IF ERRORLEVEL 5 GOTO encrypt
-IF ERRORLEVEL 4 GOTO install-apps
-IF ERRORLEVEL 3 GOTO join-azure-domain
-IF ERRORLEVEL 2 GOTO join-local-domain
-IF ERRORLEVEL 1 GOTO ren-exp
+IF ERRORLEVEL 7 GOTO backup-key
+IF ERRORLEVEL 6 GOTO encrypt
+IF ERRORLEVEL 5 GOTO install-apps
+IF ERRORLEVEL 4 GOTO join-azure-domain
+IF ERRORLEVEL 3 GOTO join-local-domain
+IF ERRORLEVEL 2 GOTO ren-exp
+IF ERRORLEVEL 1 GOTO updatescripts
 
 :backup-key
 SET PowerShellScriptPath=%ThisScriptsDirectory%backup-key.ps1
@@ -50,5 +52,10 @@ GOTO End
 
 :ren-exp
 SET PowerShellScriptPath=%ThisScriptsDirectory%ren-exp.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+GOTO End
+
+:setupscripts
+SET PowerShellScriptPath=%ThisScriptsDirectory%setupscripts.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
 GOTO End
