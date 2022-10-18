65d274586e028020f0bed86efc42b6d482d6aaef
MattWrightIWF <111991781+MattWrightIWF@users.noreply.github.com>
2022-10-18 13:31:59 +0100

    Simons changes to install-apps

--- install-apps.ps1
+++ install-apps.ps1
@@ -6,8 +6,6 @@ $credential = New-Object System.Management.Automation.PsCredential($user,$passwo
 $approotlan = "\\iwf-itserv\software"
 $approot = "software:"
 
-
-
 Write-Host "This script will install applications relevant to this PC's use"
 Write-Host "Please ensure you're connected to the OFFICE network and $approotlan is accessible"
 Write-Host "1 - Hotline Desktop"
@@ -20,48 +18,58 @@ $purpchoice = Read-Host "Please enter 1 to 4:"
 
 New-PSDrive -Name "software" -Root "$($approotlan)" -PSProvider "FileSystem" -Credential $credential
 
-& 'C:\Windows\notepad.exe' "$approot\keys.txt"
-
 if ($purpchoice -eq '1') {
 Write-Host "Setting up as Hotline desktop...."
 $softwarelocation = "$approot\hotline-n-office", "$approot\hotline"
+$ninitename = "$approot\ninite\hotline-n-office"
 }
 
 
 if ($purpchoice -eq '2') {
 Write-Host "Setting up as Office on site..."
 $softwarelocation = "$approot\hotline-n-office", "$approot\office"
+$ninitename = "$approot\ninite\hotline-n-office"
 }
 
 
 if ($purpchoice -eq '3') {
 Write-Host "Setting up as Office roaming..."
 $softwarelocation = "$approot\hotline-n-office", "$approot\office", "$approot\roaming"
+$ninitename = "$approot\ninite\hotline-n-office", "$approot\ninite\roaming"
 }
 
 
 if ($purpchoice -eq '4') {
 Write-Host "Setting up as Airgap machine..."
 $softwarelocation = "$approot\airgap"
+$ninitename = "$approot\ninite\airgap"
 }
 
 if ($purpchoice -eq '5') {
-  Write-Host "Setting up as Power User machine..."
-  $softwarelocation = "$approot\hotline-n-office", "$approot\hotline", "$approot\office", "$approot\roaming", "$approot\poweruser"
+Write-Host "Setting up as Power User machine..."
+$softwarelocation = "$approot\hotline-n-office", "$approot\hotline", "$approot\office", "$approot\roaming", "$approot\poweruser"
+$ninitename = "$approot\ninite\hotline-n-office", "$approot\ninite\poweruser"
 }
 
+& 'C:\Windows\notepad.exe' "$approot\keys.txt"
+
+$ninFiles = Get-ChildItem -Path $ninitename -Recurse -Include *.exe
+foreach ( $ninfile in $ninFiles ) {
+  $ninfullPath = $ninfile.FullName
+  Write-Host "Installing '$ninfullPath'"
+  Start-Process -FilePath $ninfullPath -NoNewWindow  -ArgumentList -Wait
+  Write-Host "$ninfullPath is finished being installed"
+}
 
 $exeFiles = Get-ChildItem -Path $softwarelocation -Recurse -Include *.exe
-
 foreach ( $exefile in $exeFiles ) {
   $exefullPath = $exefile.FullName
   Write-Host "Installing '$exefullPath'"
-  Start-Process -FilePath $exefullPath -NoNewWindow  -ArgumentList '/install' -Wait
+  Start-Process -FilePath $exefullPath -NoNewWindow  -ArgumentList '/silent', '/install' -Wait
   Write-Host "$exefullPath is finished being installed"
 }
 
 $msiFiles = Get-ChildItem -Path $softwarelocation -Recurse -Include *.msi
-
 foreach ( $msifile in $msiFiles ) {
   $msifullPath = $msifile.FullName
   Write-Host "Installing '$msifullPath'"
